0f6c9dd54b02875f67ad945d4ea338b38de65b54
Andrey Shamis <lolnik@gmail.com>
2017-01-31 21:25:10 +0200

    Some changes
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ def main_dva():
     # construct the argument parser and parse the arguments
     ap = argparse.ArgumentParser()
     ap.add_argument("-v", "--video", help="path to the video file")
-    ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
+    ap.add_argument("-a", "--min-area", type=int, default=5000, help="minimum area size")
     args = vars(ap.parse_args())
     pprint(args)
     # if the video argument is None, then we are reading from webcam
@@ -133,15 +133,20 @@ def main_dva():
     else:
         camera = cv2.VideoCapture(args["video"])
 
+    IMAGE_SIZE=400
     # initialize the first frame in the video stream
     firstFrame = None
-
+    #(grabbed, firstFrame) = camera.read()
+    #firstFrame = imutils.resize(firstFrame, width=IMAGE_SIZE)
     #pprint(camera)
     # loop over the frames of the video
+    count = 0
+    trashhold=0
     while True:
+        count=count+1
         # grab the current frame and initialize the occupied/unoccupied
         # text
-        (grabbed, frame) = camera.read()
+        (grabbed, rframe) = camera.read()
         text = "Unoccupied"
 
         # if the frame could not be grabbed, then we have reached the end
@@ -152,12 +157,13 @@ def main_dva():
             break
 
         # resize the frame, convert it to grayscale, and blur it
-        frame = imutils.resize(frame, width=400)
+        frame = imutils.resize(rframe, width=IMAGE_SIZE)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray, (21, 21), 0)
         #print "Here2"
         # if the first frame is None, initialize it
         if firstFrame is None:
+            print ("Set first frame to be gray")
             firstFrame = gray
             continue
         # compute the absolute difference between the current frame and
@@ -182,19 +188,31 @@ def main_dva():
             # compute the bounding box for the contour, draw it on the frame,
             # and update the text
             (x, y, w, h) = cv2.boundingRect(c)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 4)
             text = "Occupied"
 
-
+        time_str = datetime.now().strftime("%A %d %B %Y %I:%M:%S%p")
 
         # draw the text and timestamp on the frame
-        cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-        cv2.putText(frame, datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-                    (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+        cv2.putText(frame, "STAT: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        cv2.putText(frame, time_str, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 
         # show the frame and record if the user presses a key
         cv2.imshow("Security Feed", frame)
+        if text == "Occupied":
+            trashhold=trashhold+1
+            if trashhold>70:
+                trashhold = 0
+            try:
+                if trashhold > 30 and trashhold%20==0:
+                    cv2.imwrite(os.path.join("/home/werd/dev/camera_motion/pic_detect", '%d.jpg') % count, frame)
+                    cv2.imwrite(os.path.join("/home/werd/dev/camera_motion/pic_detect", '%d_delta.jpg') % count, frameDelta)
+                    telegram_send_all_images("pic_detect", True)
+            except:
+                pass
+        else:
+            trashhold=0
+
         cv2.imshow("Thresh", thresh)
         cv2.imshow("Frame Delta", frameDelta)
         cv2.imshow("new Frame", newFrame)
@@ -206,10 +224,10 @@ def main_dva():
         if text == "Occupied":
             try:
                 pass
-                #firstFrame = frame
+                #firstFrame = rframe
             except:
                 pass
-            print ("Here")
+            #print ("Here")
     # cleanup the camera and close any open windows
     camera.release()
     cv2.destroyAllWindows()
