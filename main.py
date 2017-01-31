8d694dbdbbf31021239f9bf6d9025631971fced4
Andrey Shamis <lolnik@gmail.com>
2017-01-31 18:20:09 +0200

    Some changes
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- main.py
+++ main.py
@@ -22,31 +22,26 @@ from pprint import pprint
 #     "socks": socks_proxy
 # }
 
+# werd_chat_id = -1001117363466
+# milena =
+# andrey =290710006
 chat_id_value = '290710006'
 token = '248037479:AAELGWsUIUkrhTTRpkotL1j2P--26Sfkov4'
 
 
 def telegram_photo_file(file_path):
-    # werd_chat_id = -1001117363466
-    # milena =
-    # andrey =290710006
     bot = telepot.Bot(token)
     bot.sendPhoto(chat_id=chat_id_value, photo=open(file_path, 'rb'))
 
 
 def telegram_message_file(file_path):
-    # werd_chat_id = -1001117363466
-    # milena =
-    # andrey =290710006
     bot = telepot.Bot(token)
     bot.sendPhoto(chat_id=chat_id_value, photo=open(file_path, 'rb'))
 
 
 def telegram_send_all_images(folder,remove=False):
     l = glob.iglob(folder+'/*.jpg')
-    bot = telepot.Bot(token)
     for t in l:
-        #bot.sendPhoto(chat_id=chat_id_value, photo=open(t, 'rb'))
         telegram_photo_file(t)
         file_name = os.getcwd() + "/" + t
         pprint(file_name)
@@ -78,7 +73,7 @@ def main():
     # Telegram_send('/home/werd/milena')
     # exit()
     try:
-        camera = cv2.VideoCapture('/dev/video0')
+        #camera = cv2.VideoCapture('/dev/video0')
         pygame.camera.init()
         pygame.camera.list_cameras()
         # cam = pygame.camera.Camera("/dev/video0", (640, 480))
@@ -88,7 +83,7 @@ def main():
             # time.sleep(0.1)
             if 1 == 1:  # pir.motion_detected:
                 cam.start()
-                time.sleep(0.2)  # You might need something higher in the beginning
+                time.sleep(0.1)  # You might need something higher in the beginning
                 time_now = datetime.utcnow().strftime(patt)
                 #print("Motion detected at " + str(time_now))
                 #if not camera.isOpened():
@@ -105,11 +100,11 @@ def main():
                 cam.stop()
                 #cv2.imwrite(file + str(time.strftime("%Y%m%d-%H%M%S")) + '.jpg', image)
                 counter += 1
-
+                telegram_send_all_images("pics/", True)
         if counter >= threshhold: telegram_send_all_images("pics/",True)
     except Exception as e:
         print('Something is wrong.', e)
-        camera.release()
+        #camera.release()
 
 
 if __name__ == "__main__":
